41a7b38b275df7c3037234b74017c23051292e2b
laxmi <laxmi.s@schoolforschools.ai>
2025-12-16 12:49:39 +0530

    --

--- src/config/api.ts
+++ src/config/api.ts
@@ -2,4 +2,4 @@
 // If in local environment, uses VITE_LOCAL_URL, otherwise uses VITE_LIVE_URL from env
 
 
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+export const API_BASE_URL =import.meta.env.VITE_LIVE_URL;
